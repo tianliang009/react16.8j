b6ef1cd311b50461550482426a56e4844a1303e5
tianliang009 <>
2024-06-18 10:52:25 +0800

    refactor: 图表构建

--- src/components/AppEchart.jsx
+++ src/components/AppEchart.jsx
@@ -10,13 +10,35 @@ const AppEchart = () => {
         getMap.then(res => {
             echarts.registerMap('CC', res.data)
             option = {
+                visualMap: {
+                    min: 1000,
+                    max: 30000,
+                    realtime: false,
+                    calculable: true,
+                    inRange: {
+                      color: ['lightskyblue', 'yellow', 'orangered']
+                    }
+                },
+                title:{ text:'长春市统计图' }, // 标题
+                tooltip: {}, // 提示框
                 series: [{
                     name: '地图',
                     type: 'map',
                     map: 'CC',
                     label: {
                         show: true
-                    }
+                    },
+                    data: [
+                        { name: "白城市", value: 10000 },
+                        { name: "松原市", value: 20000 },
+                        { name: "长春市", value: 30000 },
+                        { name: "四平市", value: 1 },
+                        { name: "辽源市", value: 5000 },
+                        { name: "吉林市", value: 4000 },
+                        { name: "通化市", value: 12001 },
+                        { name: "白山市", value: 666 },
+                        { name: "延边朝鲜族自治州", value: 777 },
+                    ]
                 }]
             }
             MixChart.resize()
